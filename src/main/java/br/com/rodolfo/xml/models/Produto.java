d20aaa0249fa7ce8238d488515e188e84a5f8122
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2018-06-16 15:49:32 -0300

    Tópico 1, Iniciando o Parse do XML com W3C DOM

---
+++ src/main/java/br/com/rodolfo/xml/models/Produto.java
@@ -0,0 +1,49 @@
+package br.com.rodolfo.xml.models;
+
+public class Produto {
+
+    private String nome;
+    private double preco;
+
+    public Produto() {}
+
+    public Produto(String nome, double preco) {
+
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    /**
+     * @return String return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return double return the preco
+     */
+    public double getPreco() {
+        return preco;
+    }
+
+    /**
+     * @param preco the preco to set
+     */
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Produto : %s - R$%.2f", this.nome, this.preco);
+    }
+
+}
---
+++ src/main/java/br/com/rodolfo/xml/test/Sistema.java
@@ -0,0 +1,51 @@
+package br.com.rodolfo.xml.test;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+//utilizando o padrão e especificações do w3c
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import br.com.rodolfo.xml.models.Produto;
+
+
+public class Sistema {
+
+    public static void main(String[] args) throws Exception {
+        //Instanciando uma fFábrica de documentos
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        //Construtor de documentos
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        //Carregamento do documento na memória e atribuído à variável. O 'document' é a TAG mais externa do xml, ou seja, é a raiz. Sendo assim, a TAG raiz do xml é a <venda></venda>
+        Document documento = builder.parse("./src/main/java/br/com/rodolfo/xml/vendas.xml");
+        
+        //Para acessar a TAG raiz (<venda></venda>)
+        Element raiz = documento.getDocumentElement();
+        //Para acesar um atributo de uma TAG
+        System.out.println(raiz.getAttribute("moeda"));
+
+        //Coo no XML pode ter várias tags com o mesmo nome o método 'getElementsByTagName' trará uma lista de tags
+        NodeList produtos = documento.getElementsByTagName("produto");
+
+        for (int i = 0; i < produtos.getLength(); i++) {
+            
+            //O método 'item' retorn um tipo 'Node' mas este tipo não possui bons métodos para se trabalhar.
+            //Utilizando a interface 'Element' fica melhor para tratamento dos dados do XML
+            Element produto = (Element) produtos.item(i);
+    
+            String nome = produto.getElementsByTagName("nome").item(0).getTextContent();
+            Double preco = Double.parseDouble(
+                                produto.getElementsByTagName("preco").item(0).getTextContent()
+                            );
+
+            Produto prod = new Produto(nome, preco);
+
+            System.out.println(prod);
+
+        }
+
+    }
+
+}
